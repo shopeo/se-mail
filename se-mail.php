2e56b71c65338e6265f8e12401ea1fad5e94f846
jabber <2898117012@qq.com>
2022-11-28 02:28:09 +0800

    init

---
+++ se-mail.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: SE Mail
+ * Plugin URI: https://wordpress.org/plugins/se-mail
+ * Description: Configure WordPress SMTP and log and disable
+ * Author: Shopeo
+ * Version: 0.0.1
+ * Author URI: https://shopeo.cn
+ * License: GPL2+
+ * Text Domain: se-mail
+ * Domain Path: /languages
+ * Requires at least: 5.9
+ * Requires PHP: 5.6
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
